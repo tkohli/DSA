a3635830d67dfa17b8c5fd6897e6600330f4ca73
Tanishka Kohli <tanishka.kohli00@gmail.com>
2021-06-10 21:45:09 +0530

    Create 47. The K Weakest Rows in a Matrix.py

---
+++ 47. The K Weakest Rows in a Matrix.py
@@ -0,0 +1,64 @@
+# The K Weakest Rows in a Matrix
+"""
+You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+
+A row i is weaker than a row j if one of the following is true:
+
+The number of soldiers in row i is less than the number of soldiers in row j.
+Both rows have the same number of soldiers and i < j.
+Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+ 
+
+Example 1:
+
+Input: mat = 
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]], 
+k = 3
+Output: [2,0,3]
+Explanation: 
+The number of soldiers in each row is: 
+- Row 0: 2 
+- Row 1: 4 
+- Row 2: 1 
+- Row 3: 2 
+- Row 4: 5 
+The rows ordered from weakest to strongest are [2,0,3,1,4].
+"""
+
+mat = [[1, 1, 0, 0, 0],
+       [1, 1, 1, 1, 0],
+       [1, 0, 0, 0, 0],
+       [1, 1, 0, 0, 0],
+       [1, 1, 1, 1, 1]]
+
+k = 3
+def kWeakestRows(self, mat, k):
+        """
+        :type mat: List[List[int]]
+        :type k: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i in range(len(mat)):
+            dict[i] = mat[i].count(1)
+        print(dict)
+        a = (sorted(dict, key=dict.get))
+        return a[:k]
+        # mat = (sorted(range(len(mat)), key=lambda k: mat[k]))
+        # return mat[:k]
+        # out = []
+        # lst =[]
+        # total = len(mat[0])
+        # for m, i in zip(mat, range(len(mat))):
+        #     s = m.count(1)
+        #     c = total-s
+        #     out.append([s-c, i])
+        # out = sorted(out, key=lambda x: x[0])
+        # for i in range(k):
+        #     lst.append(out[i][1])
+        # return lst
