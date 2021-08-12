b746753e303bb79a1746ca81f61fec67206e06c3
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-08-12 20:17:03 +0530

    undo test

--- student.py
+++
@@ -1,8 +0,0 @@
-# making a class Teacher and the making multiple objects
-from teachers import Teacher
-class Students(Teacher):
-    def setmarks(self,marks):
-        self.marks = marks
-    def getmarks(self):
-        return self.marks
-    
