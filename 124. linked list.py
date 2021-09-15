674d2722e96fb93dc4cadd5aed2fd23d1c73d1bf
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-09-15 21:17:26 +0530

    Update 124. linked list.py

--- 124. linked list.py
+++ 124. linked list.py
@@ -8,13 +8,41 @@ often represented as
 Where 1 is node.val and node.next points to next node which is node.next.val = 2
 """
 
+# class Node:
+#     def __init__(self,data):
+#         self.data = data
+#         self.next = None
+
+# class Linkedlist:
+#     def __init__(self):
+#         self.head = None
+    
 class Node:
-    def __init__(self,data):
+    def _init_(self, data):
         self.data = data
         self.next = None
 
-class Linkedlist:
-    def __init__(self):
+
+class LinkedList:
+    def _init_(self):
         self.head = None
-    
-        
+
+    def insert(self, val):
+        new = Node(val)
+        h = self.head
+        while h.next:
+            h = h.next
+        h.next = new
+
+    def traverse(self):
+        h = self.head
+        while h:
+            print(h.data)
+            h = h.next
+
+
+ll = LinkedList()
+ll.head = Node(4)
+ll.insert(5)
+ll.insert(56)
+ll.traverse()    
