b0d532bda46822539c9e49b897cd5b3240e73b6f
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-11-03 20:57:32 +0530

    Update 159. BST Construction.py

--- 159. BST Construction.py
+++ 159. BST Construction.py
@@ -3,6 +3,7 @@ class node:
         self.value = value
         self.left = None
         self.right = None
+
     def insert(self,newValue):
         if self.value:
             if self.value<newValue:
@@ -38,14 +39,50 @@ class node:
         if self.right:
             self.right.printTree()
 
+    def remove(self,value,parentNode = None):
+        if value<self.value:
+            if self.value is not None:
+                self.left.remove(value,self)
+        elif value > self.value:
+            if self.right is not None:
+                self.right.remove(value,self)
+        # now we are at the value
+        else:
+            if self.left is not None and self.right is not None:
+                # Now we need to find min of this sub tree
+                self.value = self.right.getMinValue()
+                self.right.remove(self.value,self)
+            elif parentNode is not None:
+                if self.left is not None:
+                    self.value = self.left.value
+                    self.right = self.left.right
+                    self.left = self.left.left
+                elif self.right is not None:
+                    self.value = self.right.value
+                    self.left = self.right.left
+                    self.right = self.right.right
+                else:
+                    pass # Last Node or no child 
+            elif parentNode.left == self:
+                parentNode.left = self.left if self.left is not None else self.right
+            elif parentNode.right == self:
+                parentNode.right = self.left if self.left is not None else self.right
+        return self
 
+    
+    def getMinValue(self):
+        if self.left is None:
+            return self.value
+        else:
+            return self.left.getMinValue()
+            
 root = node(27)
 root.insert(14)
 root.insert(35)
 root.insert(31)
 root.insert(10)
 root.insert(19)
-
+root.remove(35)
 
 root.printTree()
 print(root.findVal(35))
