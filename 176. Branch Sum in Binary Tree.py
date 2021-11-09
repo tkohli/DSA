1b1284343f49860f658352e6bfbfa65d5851fe17
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-11-09 10:30:53 +0530

    Create 176. Branch Sum in Binary Tree.py

---
+++ 176. Branch Sum in Binary Tree.py
@@ -0,0 +1,24 @@
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def branchSums(root):
+    sums=[]
+    calculateBranchSum(root,0,sums)
+    return sums
+
+def calculateBranchSum(node,runningSum,sums):
+    if node is None:
+        return
+    newRunningSum = runningSum+node.value
+    if node.left is None and node.right is None:
+        sums.append(newRunningSum)
+        return
+    else:
+		calculateBranchSum(node.left,newRunningSum,sums)
+        calculateBranchSum(node.right,newRunningSum,sums)
+        
