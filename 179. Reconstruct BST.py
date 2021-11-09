566c952bedcc5df3b67b800ed60fc896886f3166
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-11-09 21:38:05 +0530

    Update 179. Reconstruct BST.py

--- 179. Reconstruct BST.py
+++ 179. Reconstruct BST.py
@@ -7,8 +7,20 @@ class BST:
 
 
 def reconstructBst(preOrderTraversalValues):
-    # Write your code here.
-    return None
+    if len(preOrderTraversalValues) == 0:
+        return None
+    currentValue = preOrderTraversalValues[0]
+    rightSubTreeRootIdx = len(preOrderTraversalValues)
+
+    for idx in range(1,len(preOrderTraversalValues)):
+        value = preOrderTraversalValues[idx]
+        if value >= currentValue:
+            rightSubTreeRootIdx = idx
+            break
+
+    leftSubTree = reconstructBst(preOrderTraversalValues[1:rightSubTreeRootIdx])
+    rightSubTree = reconstructBst(preOrderTraversalValues[rightSubTreeRootIdx:])
+    return BST(currentValue,leftSubTree,rightSubTree)
 
 
 
