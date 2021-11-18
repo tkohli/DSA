b636bb44f9f1b689dc400ad0160020ae6eb7e53f
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-11-18 22:47:20 +0530

    Update 195. Levenshtein Distance.py

--- 195. Levenshtein Distance.py
+++ 195. Levenshtein Distance.py
@@ -1,7 +1,18 @@
+# O(nm) time | O(nm) space
 def levenshteinDistance(str1, str2):
     edits = [[x for x in range(len(str1)+1)] for y in range(len(str2)+1)]
-    # Write your code here.
-    return edits
+    for i in range(1,len(str2)+1):
+        edits[i][0] = edits[i-1][0]+1
+    for i in range(1,len(str2)+1):
+        for j in range(1,len(str1)+1):
+            if str1[j-1] == str2[i-1]:
+                edits[i][j] = edits[i-1][j-1]
+            else:
+                edits[i][j]= 1+min(edits[i][j-1],edits[i-1][j],edits[i-1][j-1])
+
+
+    
+    return edits[-1][-1]
 
 
 print(levenshteinDistance("abc","yabd"))
