24a198e839e8954e81c101abb2d114c97f2645a1
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-11-21 15:25:42 +0530

    Create 201. def longestCommonSubsequence.py

---
+++ 201. def longestCommonSubsequence.py
@@ -0,0 +1,12 @@
+# O(nm*min(m,n)) time | O(nm*min(m,n)) space
+
+def longestCommonSubsequence(str1, str2):
+    lcs = [[[] for x in range(len(str1)+1)] for y in range(len(str2)+1)]
+    for i in range(1,len(str2)+1):
+        for j in range(1,len(str1)+1):
+            if str2[i-1]==str1[j-1]:
+                lcs[i][j] = lcs[i-1][j-1]+[str[i-1]] 
+            else:
+                lcs[i][j] = max(lcs[i-1][j],lcs[i][j-1],key=len)
+    return lcs[-1][-1]
+
