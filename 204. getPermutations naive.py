c3c8465b819d6fc1cc26f5ff64d56e65e8c402cf
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-11-23 11:38:09 +0530

    Update 204. getPermutations naive.py

--- 204. getPermutations naive.py
+++ 204. getPermutations naive.py
@@ -1,3 +1,16 @@
+# O(n*n*n!) Time and O(n*n!) Space 
 def getPermutations(array):
-    # Write your code here.
-    pass
+    permutations = []
+    permutationsHelper(array, [], permutations)
+    return permutations
+
+def permutationsHelper(array, currentPermutations, permutations):
+    if not len(array) and len(currentPermutations):
+        permutations.append(currentPermutations)
+    else:
+        for i in range(len(array)):
+            newArray = array[:i]+array[i+1:]
+            newPermutations = currentPermutations+[array[i]]
+            permutationsHelper(newArray,newPermutations,permutations)
+    
+print(getPermutations([1,2,3]))
