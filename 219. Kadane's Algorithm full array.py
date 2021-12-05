d3281e4017e680cb3f3f288b2d99b548b9097b93
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-05 16:40:01 +0530

    Update 219. Kadane's Algorithm full array.py

--- 219. Kadane's Algorithm full array.py
+++ 219. Kadane's Algorithm full array.py
@@ -5,18 +5,12 @@ def kadanesAlgorithm(array):
     maxEndingHere = array[0]
     for i in range(1,len(array)):
         num = array[i]
-        if maxEndingHere+num>num:
-            maxEndingHere+=num
-            end=i
-        else:
+        maxEndingHere = max(num,maxEndingHere+num)
+        if maxEndingHere==num:
             start = i
-            maxEndingHere = num
-
-        # maxEndingHere = max(num,maxEndingHere+num)
-        if maxSoFar<maxEndingHere:
-            maxSoFar = maxEndingHere
-            end=i
-        # maxSoFar = max(maxSoFar,maxEndingHere)
-    return array[start:end-1],maxSoFar
+        maxSoFar = max(maxSoFar,maxEndingHere)
+        if maxSoFar==maxEndingHere:
+            end = i
+    return array[start:end+1],maxSoFar
 
 print(kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]))
