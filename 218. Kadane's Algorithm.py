2b04bf40170ef3f5d9ce19cdb5c3bbffece556a2
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-05 16:32:57 +0530

    Update 218. Kadane's Algorithm.py

--- 218. Kadane's Algorithm.py
+++ 218. Kadane's Algorithm.py
@@ -1,7 +1,7 @@
 def kadanesAlgorithm(array):
     maxSoFar = array[0]
     maxEndingHere = array[0]
-    for num in array[:]:
-        maxEndingHere = max(maxEndingHere,maxEndingHere+num)
+    for num in array[1:]:
+        maxEndingHere = max(num,maxEndingHere+num)
         maxSoFar = max(maxSoFar,maxEndingHere)
     return maxSoFar
