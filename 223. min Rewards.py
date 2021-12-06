96e9888d939462a2eb269aa1698acd4c15271f68
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-06 22:27:48 +0530

    Update 223. min Rewards.py

--- 223. min Rewards.py
+++ 223. min Rewards.py
@@ -1,3 +1,9 @@
 def minRewards(scores):
-    # Write your code here.
-    pass
+    rewards = [1 for _ in range(len(scores))]
+    for i in range(len(scores)):
+        if scores[i]>scores[i-1]:
+            rewards[i]= rewards[i-1]+1
+    for i in reversed(range(len(scores)-1)):
+        if scores[i]>scores[i+1]:
+            rewards[i] = max(rewards[i],rewards[i+1]+1)
+    return rewards
