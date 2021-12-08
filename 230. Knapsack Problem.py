ebd36c56fb749303eaeedeaee0a1b4fa17f416b4
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-08 19:50:52 +0530

    Create 230. Knapsack Problem.py

---
+++ 230. Knapsack Problem.py
@@ -0,0 +1,33 @@
+def knapsackProblem(items, capacity):
+    knapsackValues = [[0 for x in range(capacity+1)] for y in range(len(items)+1)]
+    for i in range(1,len(items)+1):
+        currentValue = items[i-1][0]
+        currentWeight = items[i-1][1]
+        for c in range(0,capacity+1):
+            if currentWeight>c:
+                knapsackValues[i][c] = knapsackValues[i-1][c]
+            else:
+                knapsackValues[i][c] = max(knapsackValues[i-1][c],
+        knapsackValues[i-1][c-currentWeight]+currentValue)
+    return [knapsackValues[-1][-1],getKnapsackItems(knapsackValues,items)]
+
+    # return [
+    #   10, // total value
+    #   [1, 2], // item indices
+    # ]
+    pass
+
+def getKnapsackItems(knapsackValues,items):
+    sequence = []
+    i = len(knapsackValues)-1
+    c = len(knapsackValues[0])-1
+    while i > 0:
+        if knapsackValues[i][c] == knapsackValues[i-1][c]:
+            i-=1
+        else:
+            sequence.append(i-1)
+            c-= items[i-1][1]
+			i-=1
+        if c==0:
+            break
+    return list(reversed(sequence))
