8fad1f4af55577e93beecac972cf6b04ac79845c
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-11 14:55:39 +0530

    Create 245. Right smaller thank.py

---
+++ 245. Right smaller thank.py
@@ -0,0 +1,12 @@
+def rightSmallerThan(array):
+    rightSmallerCounts = []
+    for i in range(len(array)):
+        rightSmallerCount = 0
+        for j in range(i+1, len(array)):
+            if array[j] < array[i]:
+                rightSmallerCount += 1
+        rightSmallerCounts.append(rightSmallerCount)
+    return rightSmallerCounts
+    """
+    To do is to improvise on this array
+    """
