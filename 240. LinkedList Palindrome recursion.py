8ca2fd6b239eb48b797a62ee12d0fa54689e1410
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-11 14:55:33 +0530

    Create 240. LinkedList Palindrome recursion.py

---
+++ 240. LinkedList Palindrome recursion.py
@@ -0,0 +1,16 @@
+# This is an input class. Do not edit.
+"""
+make recursive calles to next right node 
+check all outer nodes and then move inside 
+check again and so on...
+"""
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def linkedListPalindrome(head):
+    pass
