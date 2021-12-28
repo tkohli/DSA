c1f827eb78432335eda17ac2d1a211c5ff5cd3be
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-28 15:13:22 +0530

    Update 143. Merge Overlapping intervals.py

--- 143. Merge Overlapping intervals.py
+++ 143. Merge Overlapping intervals.py
@@ -1,15 +1,18 @@
 def mergeOverlappingIntervals(intervals):
-	sortedIntervals = sorted(intervals,key=lambda  x : x[0] )
-	currentInterval = sortedIntervals[0]
-	out = []
-	out.append(currentInterval)
-	for interval in sortedIntervals:
-		currentStart, currentEnd = currentInterval
-		nextStart, nextEnd = interval
-		if currentEnd >= nextStart:
-			currentInterval[1] = max(currentEnd,nextEnd)
-		else:
-			currentInterval = interval
-			out.append(currentInterval)
-	return (out)
-    
+    sortedIntervals = sorted(intervals, key=lambda x: x[0])
+    currentInterval = sortedIntervals[0]
+    out = []
+    out.append(currentInterval)
+    for interval in sortedIntervals:
+        currentStart, currentEnd = currentInterval
+        nextStart, nextEnd = interval
+        if currentEnd >= nextStart:
+            currentInterval[1] = max(currentEnd, nextEnd)
+        else:
+            currentInterval = interval
+            out.append(currentInterval)
+    return (out)
+
+
+# intervals = [1,3],[2,6],[8,10],[15,18]
+print(mergeOverlappingIntervals([[1, 3], [2, 6], [8, 10], [15, 18]]))
