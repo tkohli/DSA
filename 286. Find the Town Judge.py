cd9e67562cabce1e7b43835284e0df6fe59e5cce
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-01-03 10:48:20 +0530

    Update 286. Find the Town Judge.py

--- 286. Find the Town Judge.py
+++ 286. Find the Town Judge.py
@@ -8,16 +8,28 @@ class Solution(object):
         :type trust: List[List[int]]
         :rtype: int
         """
-        a = {}
-        dct = defaultdict(list)
-        judgeTrust = defaultdict(lambda: 0, a)
-        for i in range(len(trust)):
-            dct[trust[i][0]].append(trust[i][1])
-            judgeTrust[trust[i][1]] += 1
-        # print(dct, judgeTrust)
-        judge = -1
-        for i in range(1, n+1):
-            if dct[i] == []:
-                if judgeTrust[i] == n-1:
-                    return i
+        if n == 1:
+            return 1
+        out = [0 for x in range(n+1)]
+        for person, t in trust:
+            out[person] -= 1
+            out[t] += 1
+        for i in range(1, len(out)):
+            if out[i] == n-1:
+                return i
         return -1
+
+        # O(n) time and o(n) space but logically taking more time
+        # a = {}
+        # dct = defaultdict(list)
+        # judgeTrust = defaultdict(lambda: 0, a)
+        # for i in range(len(trust)):
+        #     dct[trust[i][0]].append(trust[i][1])
+        #     judgeTrust[trust[i][1]] += 1
+        # # print(dct, judgeTrust)
+        # judge = -1
+        # for i in range(1, n+1):
+        #     if dct[i] == []:
+        #         if judgeTrust[i] == n-1:
+        #             return i
+        # return -1
