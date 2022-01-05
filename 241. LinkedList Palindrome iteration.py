d63da435bc19256640cd98e515f6f05d22801892
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-01-05 22:33:29 +0530

    Create 241. LinkedList Palindrome iteration.py

---
+++ 241. LinkedList Palindrome iteration.py
@@ -0,0 +1,17 @@
+# This is an input class. Do not edit.
+"""
+loop from back to check 
+reverse half of linkedlist 
+and the use 2 pointer to check it 
+slow and fast pointer
+"""
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def linkedListPalindrome(head):
+    pass
