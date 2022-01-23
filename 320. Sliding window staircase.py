957c180233228cc8f6d955912797942f943bd7e1
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-01-23 18:42:31 +0530

    Update 320. Sliding window staircase.py

--- 320. Sliding window staircase.py
+++ 320. Sliding window staircase.py
@@ -1,3 +1,13 @@
 def staircaseTraversal(height, maxSteps):
-    # Write your code here.
-    return -1
+    currentNumberOfWays = 0
+    waysToTop = [1]
+
+    for currentheight in range(1, height+1):
+        windowStart = currentheight-maxSteps+1
+        windowEnd = currentheight+1
+        if windowStart >= 0:
+            currentNumberOfWays -= waysToTop[windowStart]
+        currentNumberOfWays += waysToTop[windowEnd]
+        waysToTop.append(currentNumberOfWays)
+
+    return waysToTop[height]
