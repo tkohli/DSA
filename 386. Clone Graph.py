cf0bb92f81fc803a0bb816c09068f03f9b25ba04
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-02-24 09:34:49 +0530

    Update 386. Clone Graph.py

--- 386. Clone Graph.py
+++ 386. Clone Graph.py
@@ -22,3 +22,28 @@ class Solution:
     def cloneGraph(self, node):
         nodeMap = {}
         return self.insert(node, nodeMap)
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if node is None:
+            return None
+        visited = {}
+        output = Node(node.val)
+        visited[node.val] = [output]
+        queue = [output]
+        while queue:
+            node = queue.pop()
+            for neigh in node.neighbors:
+                if neigh not in visited:
+                    visited[neigh.val] = Node(neigh.val)
+                    queue.append(neigh)
+                visited[node.val].neighbors.append(visited[neigh].val)
+        return output
