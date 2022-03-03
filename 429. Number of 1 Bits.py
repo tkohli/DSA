485ab25ed885fa0159f798d75a516a09bb9b8ecc
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-03-03 16:37:14 +0530

    Create 429. Number of 1 Bits.py

---
+++ 429. Number of 1 Bits.py
@@ -0,0 +1,9 @@
+# Hamming ditance
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            count+=1 # count += (n%2)
+            # n=n>>1
+            n &= (n-1)
+        return count
