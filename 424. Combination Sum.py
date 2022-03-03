15e0f0e40d76b2bcc56a62f2a68b4b623863db49
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-03-03 16:17:49 +0530

    Create 424. Combination Sum.py

---
+++ 424. Combination Sum.py
@@ -0,0 +1,39 @@
+"""
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+if we make a normal decision tree and the we get 
+repeated same set that sum up our target eg = [2,3,3] and [3,2,3] etc
+which is not allowed so modified tree will be like in which we stop 
+an element after it is seen
+eg =                0
+                /       \
+        [2]               [0]
+        /      \         /  \
+    [2,2]        [2]   [3]  [0]
+    /   \                   /   \
+[2,2,2] [2,2]             [5]    [0]
+So we are adding repeated element into left 
+And it continuous like that
+"""
+
+
+res = []
+candidates = [2,3,5]
+target = 8
+
+def dfs(i, cur, total):
+    if total==target:
+        res.append(cur.copy())
+        return
+    if total>target or i >= len(candidates):
+        return # our base return case
+    # now make 2 recusrive calls where add prev char to one
+    # and increase i to second part
+    dfs(i+1, cur, total)
+    cur.append(candidates[i])
+    dfs(i,cur,total+candidates[i])
+
+
+dfs(0,[],0)
+print(res)
