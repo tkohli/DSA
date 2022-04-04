f8955f285ba2c5134bd9a6bd9603d6bddbd835ab
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-04-04 17:55:22 +0530

    Update 530. Balanced Binary Tree.py

--- 530. Balanced Binary Tree.py
+++ 530. Balanced Binary Tree.py
@@ -10,9 +10,9 @@ class Solution:
             return True
         l = self.depth(root.left)
         r = self.depth(root.right)
-        return abs(l-1) > 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
+        return abs(l-r)<2 and self.isBalanced(root.left) and self.isBalanced(root.right)
 
-    def depth(self, node):
+    def depth(self,node):
         if node is None:
             return 0
-        return max(self.depth(node.left), self.depth(node.right))+1
+        return max(self.depth(node.left),self.depth(node.right))+1
