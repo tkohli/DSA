e69f7223de0258ff5645e0bd560df49194af8a4f
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-06-21 10:44:02 +0530

    Create 625. Furthest Building You Can Reach.py

---
+++ 625. Furthest Building You Can Reach.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+
+        heap = []
+
+      
+
+        for i in range(len(heights)-1):
+
+            diff = heights[i+1]-heights[i]
+
+            
+
+            if diff > 0:
+
+                heappush(heap, diff)
+
+            if len(heap) > ladders:  # we used all ladders so now check for bricks
+
+                bricks -= heappop(heap) # pop the smallest difference to maximize the ladder usage
+
+            if bricks < 0:
+
+                return i
+
+            
+
+        return len(heights)-1
