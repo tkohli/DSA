c334d0bb66d546e44e2ed482e540ab3e1f18826b
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-07-15 15:58:56 +0530

    Create 649. Max Area of Island.py

---
+++ 649. Max Area of Island.py
@@ -0,0 +1,51 @@
+grid = [[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
+
+"""
+So we are givin grid so we have to apply a DFS and then keep record of all
+visited elements which will take n space or we can replace all visited islands by 0
+"""
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def dfs(i, j):
+            if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]) or grid[i][j] == 0:
+                return 0
+            grid[i][j] = 0
+            return 1+dfs(i+1, j)+dfs(i-1, j)+dfs(i, j-1)+dfs(i, j+1)
+        ans = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]:
+                    ans = max(ans, dfs(i, j))
+
+        return ans
+
+
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        seen = set()
+        ans = 0
+        for r0, row in enumerate(grid):
+            for c0, val in enumerate(row):
+                if val and (r0, c0) not in seen:
+                    shape = 0
+                    stack = [(r0, c0)]
+                    seen.add((r0, c0))
+                    while stack:
+                        r, c = stack.pop()
+                        shape += 1
+                        for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+                            if (0 <= nr < len(grid) and 0 <= nc < len(grid[0])
+                                    and grid[nr][nc] and (nr, nc) not in seen):
+                                stack.append((nr, nc))
+                                seen.add((nr, nc))
+                    ans = max(ans, shape)
+        return ans
