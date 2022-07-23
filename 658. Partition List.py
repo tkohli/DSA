703f026a0e3af25dbc488d87bfeb4c54db4eec2f
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-07-23 11:25:31 +0530

    Create 658. Partition List.py

---
+++ 658. Partition List.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        beforeHead = before = ListNode(0)
+        afterHead = after = ListNode(0)
+
+        while head:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+            head = head.next
+        after.next = None
+        before.next = afterHead.next
+        return beforeHead.next
