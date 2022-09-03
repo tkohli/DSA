d9c62aa4b78ce99247e4fb7662a8d879d7933432
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-09-03 10:34:26 +0530

    Update 711. Numbers With Same Consecutive Differences.py

--- 711. Numbers With Same Consecutive Differences.py
+++ 711. Numbers With Same Consecutive Differences.py
@@ -31,26 +31,48 @@ highest position.
 
 class Solution:
     def numsSameConsecDiff(self, N: int, K: int) -> List[int]:
+        # if N == 1:
+        #     return [i for i in range(10)]
+
+        # ans = []
+
+        # def DFS(N, num):
+        #     # base case
+        #     if N == 0:
+        #         return ans.append(num)
+
+        #     tail_digit = num % 10
+        #     # using set() to avoid duplicates when K == 0
+        #     next_digits = set([tail_digit + K, tail_digit - K])
+
+        #     for next_digit in next_digits:
+        #         if 0 <= next_digit < 10:
+        #             new_num = num * 10 + next_digit
+        #             DFS(N-1, new_num)
+
+        # for num in range(1, 10):
+        #     DFS(N-1, num)
+
+        # return list(ans)
+
         if N == 1:
             return [i for i in range(10)]
 
-        ans = []
+        # initialize the queue with candidates for the first level
+        queue = [digit for digit in range(1, 10)]
 
-        def DFS(N, num):
-            # base case
-            if N == 0:
-                return ans.append(num)
+        for level in range(N-1):
+            next_queue = []
+            for num in queue:
+                tail_digit = num % 10
+                # using set() to avoid duplicates when K == 0
+                next_digits = set([tail_digit + K, tail_digit - K])
 
-            tail_digit = num % 10
-            # using set() to avoid duplicates when K == 0
-            next_digits = set([tail_digit + K, tail_digit - K])
+                for next_digit in next_digits:
+                    if 0 <= next_digit < 10:
+                        new_num = num * 10 + next_digit
+                        next_queue.append(new_num)
+            # start the next level
+            queue = next_queue
 
-            for next_digit in next_digits:
-                if 0 <= next_digit < 10:
-                    new_num = num * 10 + next_digit
-                    DFS(N-1, new_num)
-
-        for num in range(1, 10):
-            DFS(N-1, num)
-
-        return list(ans)
+        return queue
