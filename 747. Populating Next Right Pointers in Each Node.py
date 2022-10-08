021f86298be030374277d6ed1230b30c81a07b72
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-10-08 11:01:00 +0530

    Create 747. Populating Next Right Pointers in Each Node.py

---
+++ 747. Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,31 @@
+"""
+The idea is to implement a level order traversal and add next element to
+next pointer in Node 
+"""
+# Definition for a Node.
+
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return root
+        queue = [root]
+        while queue:
+            tmp = []
+            for node in queue:
+                if node.left:
+                    tmp.append(node.left)
+                if node.right:
+                    tmp.append(node.right)
+            for i in range(len(tmp)-1):
+                tmp[i].next = tmp[i+1]
+            queue = tmp[:]
+        return root
