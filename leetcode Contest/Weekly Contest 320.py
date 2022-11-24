47ceb55c3e1ad19b876bdf881580d3341c215251
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2022-11-24 17:02:43 +0530

    Update Weekly Contest 320.py

--- leetcode Contest/Weekly Contest 320.py
+++ leetcode Contest/Weekly Contest 320.py
@@ -1,1 +1,13 @@
 # Number of Unequal Triplets in Array
+nums = [4, 4, 2, 4, 3]
+
+
+class Solution:
+    def unequalTriplets(self, nums: List[int]) -> int:
+        cnt = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
+                        cnt += 1
+        return cnt
