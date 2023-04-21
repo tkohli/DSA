c8c128c1603a7c942dd141c99986ef912acba73a
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2023-04-21 16:30:01 +0530

    Update 849. Maximum Width of Binary Tree.py

--- 849. Maximum Width of Binary Tree.py
+++ 849. Maximum Width of Binary Tree.py
@@ -5,7 +5,54 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+"""
+EXPLANATION
+0. Pretty sure we have todo bfs, we can use 2 Queus or deque(doubly ended queue)
 
+1.  According to Problem , we have also to count null nodes b/w two nodes.
+		
+	     Tree        No of nodes
+		  5				1
+	     / \
+	    3   6			2
+	   /   /
+	  2   4				2
+
+2.  Above example we have max 2 nodes in a level, but if you see the second level you
+will find that right child of 3 is NULL and therefore we also have to count that 
+NULL node, and with that the last level now contain 3 nodes, refer to diagram below.
+
+	      5				1
+	   /     \
+	   3      6				2
+	 /  \    /
+	2  NULL  4				3 (NULL node also counted)
+
+So Last Level Contains 3 Nodes Including NULL.
+
+3 . We will also assign index to every node
+	(if we are startting index from 0)
+	
+	(below is just formula to find index of node of complete binary tree)
+	left(idx) = 2 idx + 1			// left child
+	right(idx) = 2 idx + 2			// right child
+
+	At starting root, idx = 0 so left node idx would be 2*0 +1 and right node index would be 2*0 + 2
+	Index tree would be
+
+		   0				1
+	   /     \
+	  1       2				2
+	 /  \    /
+	3    4  5				3 
+
+	maxWidth would be 5-3+1 = 3
+
+Spoilers : Use a 2D queue
+
+
+we will also insert index with node , so that we will know,
+"""
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         level = deque([(root, 0)])
