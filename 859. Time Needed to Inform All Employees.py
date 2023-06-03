d4a3b6a77bdd195271fb6ff7b1ca9e0433229d6c
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2023-06-03 18:04:54 +0530

    Create 859. Time Needed to Inform All Employees.py

---
+++ 859. Time Needed to Inform All Employees.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        if n <= 1:
+            return 0
+        rst = 0
+        childs = defaultdict(list)
+        for idx, parent in enumerate(manager):
+            childs[parent].append(idx)
+
+        q = deque([(headID, informTime[headID])])
+        while q:
+            cur_id, cur_time = q.popleft()
+            # calculate max
+            rst = max(rst, cur_time)
+            for child in childs[cur_id]:
+                q.append((child, cur_time + informTime[child]))
+        return rst
