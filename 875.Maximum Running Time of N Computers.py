930d43a3fc6b12b876352fb8ebb1e1ab5402bdaa
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2023-07-27 23:14:40 +0530

    Create 875.Maximum Running Time of N Computers.py

---
+++ 875.Maximum Running Time of N Computers.py
@@ -0,0 +1,30 @@
+# Maximum Running Time of N Computers
+
+n = 2
+batteries = [3,3,3]
+
+def maxRunTime(n, batteries):
+    # Get the sum of all extra batteries.
+    batteries.sort()   
+    extra = sum(batteries[:-n])
+    
+    # live stands for the n largest batteries we chose for n computers.
+
+    live = batteries[-n:]
+    
+    # We increase the total running time using 'extra' by increasing 
+    # the running time of the computer with the smallest battery.
+    for i in range(n - 1):
+        # If the target running time is between live[i] and live[i + 1].
+        if extra // (i + 1) < live[i + 1] - live[i]:
+            return live[i] + extra // (i + 1)
+        
+        # Reduce 'extra' by the total power used.
+        extra -= (i + 1) * (live[i + 1] - live[i])
+    
+    # If there is power left, we can increase the running time 
+    # of all computers.
+    return live[-1] + extra // n
+
+
+print(maxRunTime(n, batteries))
